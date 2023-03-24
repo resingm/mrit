b72732f7cc9a693562b4d0a65e4b170556c306bb
Max Resing <contact@maxresing.de>
2023-03-24 17:12:57 +0100

    cymru-asn.sh to bulk-lookup ASN and other information for IPs.

---
+++ cymru-asn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+host="whois.cymru.com"
+
+# Trap the temporary directory to delete on EXIT
+TMP=$(mktemp -d)
+trap "rm -rf $TMP" EXIT
+
+
+# Input file
+bulk="$TMP/bulk.txt"
+
+echo "begin" >> $bulk
+echo "verbose" >> $bulk
+# TODO: Add a filter on IPv4 and IPv6.
+cat "${1:-/dev/stdin}" | sort | uniq >> $bulk
+echo "end" >> $bulk
+
+netcat $host 43 < $bulk
+
+
+
+
+
