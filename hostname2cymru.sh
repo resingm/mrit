d81c365d2390bfc8ef8d15839f6e19cc90fa4f8f
Max Resing <contact@maxresing.de>
2023-11-23 10:30:55 +0100

    Added a hostname to IP to ASN over the CYMRU IP-to-ASN lookup

---
+++ hostname2cymru.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+host="whois.cymru.com"
+
+# Trap the temporary directory to delete on EXIT
+TMP_A=$(mktemp)
+TMP_AAAA=$(mktemp)
+
+trap "rm -rf $TMP_A" EXIT
+trap "rm -rf $TMP_AAAA" EXIT
+
+# echo $1
+fqdn="${1:-$(cat /dev/stdin)}"
+
+# Query A & AAAA records
+dig +short A "${fqdn}" > "$TMP_A"
+dig +short AAAA "${fqdn}" > "$TMP_AAAA"
+
+
+
+if [[ -s $TMP_A ]] ; then
+  echo "-----------------------------------------------------------------------------------------" 
+  whois -h "${host}" "-v $(cat ${TMP_A})" | grep -v "Warning: RIPE flags used"
+  echo ""
+else
+  # echo "" >&2
+  echo "No A record found" >&2
+fi
+
+if [[ -s "${TMP_AAAA}" ]] ; then
+  echo "-----------------------------------------------------------------------------------------------------------------"
+  whois -h "${host}" "-v $(cat ${TMP_AAAA})" | grep -v "Warning: RIPE flags used"
+  echo ""
+else
+  # echo "" >&2
+  echo "No AAAA record found" >&2
+fi
+
