4c0d9b43ce0252a7b25dba7c8bc4f9cad8ba829f
Max Resing <max.resing@netscout.com>
2024-06-26 17:13:15 +0200

    Latest changes safelink-disassembler

--- safelink-disassembler.py
+++ safelink-disassembler.py
@@ -7,18 +7,7 @@ import sys
 from urllib.parse import unquote_plus
 
 
-def disassemble_endpoint(base_url):
-    proto, r = base_url.split("://", maxsplit=1)
-    region, _ = r.split(".", maxsplit=1)
-    fqdn, _ = r.split("/", maxsplit=1)
-
-    return {
-        "protocol": proto,
-        "region": region,
-        "fqdn": fqdn,
-        "uri": base_url,
-    }
-
+link="https://eur02.safelinks.protection.outlook.com/?url=http%3A%2F%2Fwww.integrand.nl%2F&data=05%7C01%7Cs.s.spuls%40utwente.nl%7C6aa76a69803f4f56299b08dbbf69d46b%7C723246a1c3f543c5acdc43adb404ac4d%7C0%7C0%7C638314233487847100%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=f8OUXgkqULnWjhGbCmk%2FVtHiis9q%2FsbCxKeORTEaWdo%3D&reserved=0"
 
 
 def main():
@@ -33,10 +22,10 @@ def main():
     endpoint, data = args.safelink.split("?")
     params = data.split("&")
     params = [tuple(e.split("=")) for e in params if len(e) > 0]
-    params = [(e[0], unquote_plus(e[1])) for e in params]
+    params = [(e[0].replace("amp;", ""), unquote_plus(e[1])) for e in params]
 
     # Build struct around the endpoint
-    struct = { "_meta": disassemble_endpoint(endpoint) }
+    struct = { "_meta": { "endpoint": endpoint } }
     struct.update(dict(params))
 
     # Further disassembling the data
