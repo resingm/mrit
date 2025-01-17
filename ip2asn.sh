7a5a679f9bec15ad607f785c3ced1cca71e6c2a1
Max Resing <github@maxresing.de>
2025-01-17 08:45:19 +0100

    Improved the ip2asn lookup by adding command line arguments.

--- ip2asn.sh
+++ ip2asn.sh
@@ -2,6 +2,47 @@
 
 host="whois.cymru.com"
 
+usage() {
+    echo "Usage: $0 [-46] [input_file]"
+    echo ""
+    echo "This scripts performs a lookup of IP addresses. It will send a whois request"
+    echo "to Team Cymru's whois service. Besides the regular lookup, one can also lookup"
+    echo "the IP's peering networks, e.g. one hop away from the originating ASN. This is"
+    echo "handy to understand the upstream providers of a given IP address."
+    echo ""
+    echo "Options:"
+    echo "  -4      Enforces the usage of IPv4 for data transmission"
+    echo "  -p      Looks up the first hop of an IP address."
+    echo ""
+    echo "Examples:"
+    echo "  $0 -4 input_file.txt    # Performs a whois lookup by using the IPv4 service"
+    echo "  cat input_file | $0     # Performs a lookup by reading from STDIN"
+    echo ""
+    exit 1
+}
+
+# Parser command line arguments
+while getopts "4p" opt; do
+    case $opt in
+        4)
+            host="v4.whois.cymru.com"
+            ;;
+        p)
+            host="v4-peer.whois.cymru.com"
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# Shift parsed options so $1 becomes the input file if provided
+shift $((OPTIND - 1))
+
+
 # Trap the temporary directory to delete on EXIT
 TMP=$(mktemp -d)
 trap "rm -rf $TMP" EXIT
